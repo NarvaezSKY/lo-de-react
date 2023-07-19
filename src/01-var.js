1e9d3a77b897c248329584da59f59c1edbff5f5e
cristian <crissnar12xd@gmail.com>
2023-07-19 18:45:53 -0500

    so

--- src/01-var.js
+++ src/01-var.js
@@ -5,16 +5,24 @@
 // console.log (`hola, soy ${nombre} ${apellido}`)
 
 
-const materia={
-    profesor:"Javier",
-    tema: "react",
-    horas:"si"
-}
+// const materia={
+//     profesor:"Javier",
+//     tema: "react",
+//     horas:"si"
+// }
 
 
-const materia2={...materia}
-materia2.profesor="yo"
+// const materia2={...materia}
+// materia2.profesor="yo"
 
 
-console.table (materia)
-console.table (materia2)
+// console.table (materia)
+// console.table (materia2)
+
+const numeros  = [1,2,3]
+let numeros2=[...numeros]
+numeros2=numeros.map((numero)=>{
+    return numero*2
+})
+
+console.table(numeros2)
